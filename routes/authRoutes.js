5f4422cba0573721f0f08ae08338a9a4c9b3851e
La svedese <5554930+sarasjodin@users.noreply.github.com>
2026-06-17 16:08:46 +0200

    Add local Docker setup and login validation

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -26,10 +26,20 @@ router.post('/register', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    userName = req.body.username;
-    password = req.body.password;
-    console.log('Logging in user:', req.body);
-    res.status(200).json({ message: 'User logged in successfully' });
+    const { username: userName, password: pass } = req.body;
+
+    // Validation
+    if (!userName || !pass) {
+      return res
+        .status(400)
+        .json({ message: 'Username and password are required' });
+    }
+    if (userName === 'mattias' && pass === 'password') {
+      console.log('Logging in user:', req.body);
+      res.status(200).json({ message: 'User logged in successfully' });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
   } catch (error) {
     console.error('Error logging in user:', error);
     res.status(500).json({ message: 'Server error' });
