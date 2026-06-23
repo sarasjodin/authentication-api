7728cb46e04180c5f0fada974f54a5ad1f14e33c
La svedese <5554930+sarasjodin@users.noreply.github.com>
2026-06-23 10:37:51 +0200

    add comments and corrected some errors

--- install.js
+++ install.js
@@ -1,18 +1,20 @@
+/* Initialize SQLite database and create the users table. */
+
 require('dotenv').config();
-const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 
 // Connect to SQLite database
 const db = new sqlite3.Database(process.env.DATABASE_URL);
 
-// Create table users
+// Create table users if not exists
 db.serialize(() => {
   db.run(
     `
       CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username VARCHAR(255) NOT NULL UNIQUE,
-        password VARCHAR(255) NOT NULL,
+        username TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
         created DATETIME DEFAULT CURRENT_TIMESTAMP
       )
     `,
